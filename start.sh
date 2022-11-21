e474ab95b93dca5cdf770532f20a0b32d21a87f7
TheEasyLemon <dawsonren@gmail.com>
2022-11-21 15:53:24 -0600

    Add test + shell script

---
+++ start.sh
@@ -0,0 +1,2 @@
+# sets PYTHONPATH
+export PYTHONPATH="$PYTHONPATH:~/Documents/Research/NurseSim"
--- tests/test_ProblemInstance.py
+++ tests/test_ProblemInstance.py
@@ -7,11 +7,16 @@ from ProblemReader.ProblemReader import loadProblemInstance
 
 class TestProblemReader(unittest.TestCase):
     def test1_data(self):
+        pi = loadProblemInstance('tests/test1_data.txt')
+        # calculated by hand
+        self.assertAlmostEqual(pi.expectedRevenue(), 9.5365)
+
+    def test2_data(self):
         pi = loadProblemInstance('tests/test2_data.txt')
         # calculated by hand
         self.assertAlmostEqual(pi.expectedRevenue(), 4.79)
 
-    def test2_data(self):
+    def test3_data(self):
         pi = loadProblemInstance('tests/test3_data.txt')
         # calculated by hand
         self.assertAlmostEqual(pi.expectedRevenue(), 6.59)
